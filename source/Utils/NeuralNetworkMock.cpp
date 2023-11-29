5d8a2b9731ead0537d6de6b81098eddd241690ec
jorshi <j.m.shier@qmul.ac.uk>
2023-11-29 23:39:57 +0000

    NN mock simplify

--- source/Utils/NeuralNetworkMock.cpp
+++ source/Utils/NeuralNetworkMock.cpp
@@ -7,17 +7,7 @@ void NeuralNetwork::loadModel(const std::string& path)
 
 void NeuralNetwork::process(const std::vector<double>& input, std::vector<double>& output)
 {
-    if (! modelLoaded)
-    {
-        juce::Logger::outputDebugString("Could not acquire read lock");
-        std::fill(output.begin(), output.end(), 0.0);
-        return;
-    }
-
-    for (int i = 0; i < output.size(); ++i)
-    {
-        output[i] = 1.0;
-    }
+    std::fill(output.begin(), output.end(), 0.0);
 }
 
 void NeuralNetwork::_loadModel(const std::string& path)
